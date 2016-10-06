6b59569758e3a2c4cb2f2605f600870da3e6cccb
Jared Wallace <jared@jared-wallace.com>
2016-10-05 19:05:23 -0500

    Adding EPL license for IBM compliance

--- zgrep.sh
+++ zgrep.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#################################################################################
+# Copyright (c) October 5, 2016 Jared Wallace.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#    Jared Wallace- initial implementation and/or initial documentation
+#################################################################################
+
 # Need to check that the script was passed a variable (filename)
 if [ $# -lt 2 ]; then
     echo "You must provide one search term and at least one filename. For example:"
